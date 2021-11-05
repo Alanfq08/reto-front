d9b7f7c838083c6e3c7f8d5121e46e954375d7e3
alanfq08 <alanfq08@hotmail.com>
2021-11-05 15:26:28 -0600

    prueba de cors

--- src/components/Headers/ProfilePageHeader.js
+++ src/components/Headers/ProfilePageHeader.js
@@ -10,22 +10,43 @@ export default class ProfilePageHeader extends Component {
   }
 
   componentDidMount() {
-    const url = "https://cors-anywhere.herokuapp.com/https://warm-retreat-82659.herokuapp.com/redsocial/Persons/all";
-    axios.get(url)
-      .then(response => {
-        this.setState({ persons: response.data });
-      });
+    // const url = "https://cors-anywhere.herokuapp.com/https://warm-retreat-82659.herokuapp.com/redsocial/Persons/all";
+    // axios.get(url)
+    //   .then(response => {
+    //     this.setState({ persons: response.data });
+    //   });
 
     function hacer() {
       axios({
         method: 'post',
-        url: `https://cors-anywhere.herokuapp.com/https://warm-retreat-82659.herokuapp.com/redsocial/Posts/`,
+        url: `https://warm-retreat-82659.herokuapp.com/redsocial/Posts/`,
         data: {
-          user: 9,
-          text: 'ya funciona plox'
-        }
-      });
-      console.log("ahora si");
+          user: 43,
+          text: 'VAMOS AHORA SI PLOX'
+        },
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+        mode: 'no-cors',
+      })
+        .then((response) => {
+          // crossorigin:true,
+          console.log(response.status);
+        })
+        .catch((error) => {
+          console.error({ error });
+        });
+      console.log("ahora si :(");
+
+      // axios
+      //   .get(url)
+      //   .then((response) => {
+      //     console.log(response.status);
+      //   })
+      //   .catch((error) => {
+      //     console.error({ error });
+      //   });
     };
     hacer();
   }
@@ -49,6 +70,7 @@ export default class ProfilePageHeader extends Component {
             <div className="photo-container">
               <img id="fotoDePerfil" alt="Cargando..." src="/fotoPerfil.png"></img>
             </div>
+            {console.log(this.state.persons)}
             {this.state.persons.slice(0, 1).map(person => <h3 key="1" id="nombre" className="title">{person.name}</h3>)}
             {this.state.persons.slice(0, 1).map(person => <p key="2" id="descripcionBreve" className="category">{person.age}</p>)}
             <div className="content">
